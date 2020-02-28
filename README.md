6659fbd03e32d6c843963daf45a16dd596acf131
Daniel Ciborowski <dciborow@microsoft.com>
2020-02-27 21:21:20 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,73 @@
+## commit
+
+Git commit and push
+
+### Example
+
+```yaml
+name: publish
+
+on:
+  push:
+    branches:
+    - master
+    
+jobs:
+  build:
+    runs-on: ubuntu-latest
+    steps:
+    - name: checkout
+      uses: actions/checkout@master
+      with:
+        ref: master
+    - name: build
+      uses: github-actions-x/hugo@master
+    - name: push
+      uses: dciborow/commit@0.0.2
+      with:
+        github-token: ${{ secrets.GITHUB_TOKEN }}
+        push-branch: 'master'
+        commit-message: 'publish'
+        force-add: 'true'
+        force-push: 'true'
+        files: a.txt b.txt c.txt dirA/ dirB/ dirC/a.txt
+        name: commiter name
+        email: my.github@email.com 
+
+```
+
+If you use `commit` inside [matrix](https://help.github.com/en/articles/workflow-syntax-for-github-actions#jobsjob_idstrategymatrix), set variable `rebase='true'` for pulling and rebasing changes.
+
+```yaml
+name: Node CI
+
+on:
+  push:
+    branches:
+    - master
+
+jobs:
+  build:
+    runs-on: ubuntu-latest
+    strategy:
+      matrix:
+        node-version:
+          - 10.x
+          - 12.x
+    steps:
+      - uses: actions/checkout@v1
+      - name: Use Node.js ${{ matrix.node-version }}
+        uses: actions/setup-node@v1
+        with:
+          node-version: ${{ matrix.node-version }}
+      - name: generate benchmarks
+        run: |
+          npm run generate-some-files-for-specific-node-version
+      - name: push
+        uses: dciborow/commit@0.0.2
+        with:
+          github-token: ${{ secrets.GITHUB_TOKEN }}
+          push-branch: master
+          commit-message: '${{ matrix.node-version }} adds auto-generated benchmarks and bar graph'
+          rebase: 'true' # pull abd rebase before commit
+```
---
+++ entrypoint.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+
+from plumbum import local
+import os
+
+def debug(message: str):
+    print(f'##[debug]{message}')
+
+
+def run():
+    netrc_path = os.path.join(local.env.get('HOME', ''), '.netrc')
+    github_actor = local.env.get('GITHUB_ACTOR')
+    github_token = local.env.get('INPUT_GITHUB-TOKEN')
+    commit_message = local.env.get('INPUT_COMMIT-MESSAGE')
+    force_add = local.env.get('INPUT_FORCE-ADD')
+    force_push = local.env.get('INPUT_FORCE-PUSH')
+    branch = local.env.get('INPUT_PUSH-BRANCH') or local.env.get('GITHUB_REF').split('/')[2]
+    rebase = local.env.get('INPUT_REBASE', 'false')
+    files = local.env.get('INPUT_FILES', '')
+    email = local.env.get('INPUT_EMAIL', f'{github_actor}@users.noreply.github.com')
+    name = local.env.get('INPUT_NAME', github_actor)
+    with open(netrc_path, 'w') as f:
+        f.write(
+            f'machine github.com\n'
+            f'login {github_actor}\n'
+            f'password {github_token}\n'
+            f'machine api.github.com\n'
+            f'login {github_actor}\n'
+            f'password {github_token}\n'
+        )
+    chmod = local['chmod']
+    git = local['git']
+    debug(chmod(['600', netrc_path]))
+    debug(git(['config', '--global', 'user.email', email]))
+    debug(git(['config', '--global', 'user.name', name]))
+    debug(f'username:{github_actor}, branch:{branch}, commit message:{commit_message}')
+    with open(netrc_path) as f:
+        debug(f.read())
+    add_args = ['add']
+    if force_add == 'true':
+        add_args.append('-f')
+    add_args.append('-A')
+    if files:
+        add_args.append(files)
+    if rebase == 'true':
+        debug(git(['pull', '--rebase', '--autostash', 'origin', branch]))
+    debug(git(['checkout', '-B', branch]))
+    debug(git(add_args))
+    debug(git(['commit', '-m', commit_message], retcode=None))
+    push_args = ['push']
+    if force_push == 'true':
+        push_args.append('--force')
+    push_args.append('--follow-tags')
+    push_args.append('--set-upstream')
+    push_args.append('origin')
+    push_args.append(branch)
+    debug(git(push_args))
+
+if __name__ == '__main__':
+    run()
